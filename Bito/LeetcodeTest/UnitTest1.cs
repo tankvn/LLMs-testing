03698822749ba9c6513521aa099249c9c4a742dd
Tank.vn <tanvd357@gmail.com>
2024-06-17 23:32:10 +0700

    Bito - 1. Two Sum

---
+++ Bito/LeetcodeTest/UnitTest1.cs
@@ -0,0 +1,76 @@
+using NUnit.Framework;
+using LLM;
+
+namespace LeetcodeTest;
+
+public class Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	/* 1. Two Sum
+	Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+	You may assume that each input would have exactly one solution, and you may not use the same element twice.
+	You can return the answer in any order.
+
+	Example 1:
+	Input: nums = [2,7,11,15], target = 9
+	Output: [0,1]
+	Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+	Example 2:
+	Input: nums = [3,2,4], target = 6
+	Output: [1,2]
+
+	Example 3:
+	Input: nums = [3,3], target = 6
+	Output: [0,1]
+	*/
+	[Test]
+	public void TwoSum_WithValidInput_ShouldReturnCorrectIndices()
+	{
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		int[] expected = { 0, 1 };
+
+		int[] result = Program.TwoSum(nums, target);
+
+		Assert.AreEqual(expected, result);
+	}
+
+	[Test]
+	public void TwoSum_WithNoSolution_ShouldReturnNull()
+	{
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 3;
+
+		int[] result = Program.TwoSum(nums, target);
+
+		Assert.IsNull(result);
+	}
+
+	[Test]
+	public void TwoSum_WithDuplicateNumbers_ShouldReturnCorrectIndices()
+	{
+		int[] nums = { 3, 3 };
+		int target = 6;
+		int[] expected = { 0, 1 };
+
+		int[] result = Program.TwoSum(nums, target);
+
+		Assert.AreEqual(expected, result);
+	}
+
+	[Test]
+	public void TwoSum_WithEmptyArray_ShouldReturnNull()
+	{
+		int[] nums = {};
+		int target = 9;
+
+		int[] result = Program.TwoSum(nums, target);
+
+		Assert.IsNull(result);
+	}
+}
---
+++ Csharp.md
@@ -0,0 +1,34 @@
+# Getting Started with C# in VS Code
+
+## Install
+
+1. If you haven't already done so, [install VS Code](https://code.visualstudio.com).
+2. Next, install [C# Dev Kit](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csdevkit) from the Visual Studio Marketplace.
+3. Install [.NET SDK](https://dotnet.microsoft.com/en-us/download)
+
+## Create a console app
+
+The Terminal opens with the command prompt in the `Leetcode` folder.
+
+```csharp
+dotnet new console --framework net8.0 --use-program-main
+dotnet run
+```
+
+## Create a unit test project
+
+```csharp
+dotnet new mstest -o LeetcodeTest
+dotnet new nunit -o LeetcodeTest
+dotnet new xunit -o LeetcodeTest
+dotnet add ./LeetcodeTest/LeetcodeTest.csproj reference ./Leetcode/Leetcode.csproj
+dotnet test
+```
+
+Reference
+- [C# in VS Code](https://code.visualstudio.com/docs/csharp/introvideos-csharp)
+- [Tutorial: Create a .NET console application using Visual Studio Code](https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code)
+- [Tutorial: Test a .NET class library using Visual Studio Code](https://learn.microsoft.com/en-us/dotnet/core/tutorials/testing-library-with-visual-studio-code)
+- [Unit testing C# in .NET using dotnet test and xUnit](https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test)
+- [Unit testing C# with NUnit and .NET Core](https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-nunit)
+- [Unit testing C# with MSTest and .NET](https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-mstest)
